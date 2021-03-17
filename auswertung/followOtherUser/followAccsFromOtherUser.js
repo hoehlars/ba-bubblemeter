ed4e5156f12078d63e4741ff310a84ccb7d1587d
Lars Höhener <hoehelar@students.zhaw.ch>
2021-03-17 13:57:14 +0100

    Updated function to copy accounts.

--- auswertung/followOtherUser/followAccsFromOtherUser.js
+++ auswertung/followOtherUser/followAccsFromOtherUser.js
@@ -1,22 +1,7 @@
 
 
-export default async function followAccsFromOtherUser(username, password, accountNameToFollow, page) {
-  // open twitter
-  console.log('opens twitter')
-  await page.goto('https://twitter.com/login');
-        
-  // Login
-  console.log('logging in the user')
-  await page.waitForSelector('input[type="text"]')
-  await page.type('input[type="text"]', username)
-  await page.waitForSelector('input[type="password"]')
-  await page.type('input[type="password"]', password)
-  await page.click('div[role="button"]')
-        
-  // wait till page load
-  await page.waitForNavigation()
-
-  console.log(`Opening twitter page ${accountNameToFollow}`)
+export default async function followAccsFromOtherUser(accountNameToFollow, page) {
+  await page.goto('https://twitter.com/home')
   await page.goto(`https://twitter.com/${accountNameToFollow}`)
   await page.waitForSelector('div[data-testid*="follow"]')
   await page.click('div[data-testid*="follow"]')
--- auswertung/followOtherUser/index.js
+++ auswertung/followOtherUser/index.js
@@ -1,51 +1,73 @@
-
 import likeTweetFromOtherUser from './likeTweetsFromOtherUser'
 import followAccsFromOtherUser from './followAccsFromOtherUser'
 
-const { Cluster } = require('puppeteer-cluster');
+const puppeteer = require('puppeteer');
+const readXlsxFile = require('read-excel-file/node');
 
-const timeout= 15000
 
-const accountNamesToFollow = ['GabiSchmid81', 'HohenerLars']
-
-const tweetsToLike = {andreas_glarner: '1368613470078447628',
-                      SVPch: '1368951935555014660'}
-
-async function createClusterForClone(username, password) {
-    const cluster = await Cluster.launch({
-        concurrency: Cluster.CONCURRENCY_CONTEXT,
-        maxConcurrency: 2,
-        puppeteerOptions: {
-            args: ["--no-sandbox"]
-        }
-    });
-
-    accountNamesToFollow.forEach(accountName => {
-        cluster.queue(async ({ page }) => {
-            await followAccsFromOtherUser(username, password, accountName, page)
-            page.waitForTimeout(timeout)
-        });
-    })
-
-    for(const [twitterer, tweetID] of Object.entries(tweetsToLike)) {
-        cluster.queue(async ({ page }) => {
-            await likeTweetFromOtherUser(username, password, twitterer, tweetID, page)
-            page.waitForTimeout(timeout)
-        });
+// Read in excel
+readXlsxFile('Klone.xlsx').then((rows) => {
+    for(const row of rows) {
+        const clone = row[0]
+        const passwordOfClone = row[1]
+        const accountToCopy = row[2]
+        copyAccount(clone, passwordOfClone, accountToCopy).then(() => {
+            console.log(`Liked and followed everyone of ${accountToCopy} for clone ${clone}`)
+        })
     }
-    
-    await cluster.idle();
-    await cluster.close();
+})
+
+async function copyAccount(clone, passwordOfClone, accountToCopy) {
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: [
+            'no-sandbox',
+        ]
+    });
+    const page = await browser.newPage();
+
+    const followers = ['HohenerLars', 'GabiSchmid81']
+    //TODO: const followers = getAllFollowers(accountToCopy);
+
+    const likedTweets = {TobiasInfortuna: '1372101756478447618'}
+    //TODO: const likedTweets = getAllLikedTweets(accountToCopy);
+        
+    //TODO: const passwordOfClone = getPassword(clone);
+
+    // open twitter
+    await page.goto('https://twitter.com/login')
+                
+    // Login
+    await page.waitForSelector('input[type="text"]')
+    await page.type('input[type="text"]', clone)
+    await page.waitForSelector('input[type="password"]')
+    await page.type('input[type="password"]', passwordOfClone)
+    await page.click('div[role="button"]')
+    console.log(`Logged in the clone ${clone}!`)
+                
+    // wait till page load
+    await page.waitForNavigation()
+
+    for(const follower of followers) {
+        await followAccsFromOtherUser(follower, page)
+        await delay(15000)
+    }
+
+    for(const [twitterer, tweetID] of Object.entries(likedTweets)) {
+        await likeTweetFromOtherUser(twitterer, tweetID, page)
+        await delay(10000)
+    }
+    await browser.close();
 }
 
 
-const user = {GabiSchmid4: '7k0*A1xvK!af8JJh^&sW'}
-
-for(const [username, password] of Object.entries(user)) {
-    createClusterForClone(username, password).then(res => {
-        console.log(`Liked and followed everyone for user ${username}`)
-    })
+function delay(time) {
+    return new Promise(function(resolve) { 
+        setTimeout(resolve, time)
+    });
 }
+
+
 
 
 
