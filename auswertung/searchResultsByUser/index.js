d452856d9f5438a4e364585e35060e2aa2e0f67c
Daniel Lerch <daniel@daniellerch.ch>
2021-03-22 08:26:06 +0100

    better login and parameters for running the fun

--- auswertung/searchResultsByUser/index.js
+++ auswertung/searchResultsByUser/index.js
@@ -3,12 +3,15 @@ const fs = require('fs')
 const { default: getSearchResults } = require('./getSearchResults')
 import randomDelay from './helpers/randomDelay'
 
-const userName = '@RealSchmid'
-const password = '6EStjCdBnMkxaLw'
-const data = fs.readFileSync('./suchbegriffe.json')
-const { queries } = JSON.parse(data)
+const rawQueries = fs.readFileSync('./suchbegriffe.json')
+const { queries } = JSON.parse(rawQueries)
+
+// hardcoded example for dev purposes
 
 ;(async function scrape() {
+  const userName = process.argv[2] || '@RealSchmid'
+  const password = process.argv[3] || '6EStjCdBnMkxaLw'
+
   // TODO: Extract Puppeteer Setup and Twitter Login to separate, shared Functions
 
   const browser = await puppeteer.launch({
@@ -37,9 +40,9 @@ const { queries } = JSON.parse(data)
   await page.goto('https://twitter.com/login', { waitUntil: 'networkidle2' })
 
   // assure fields are loaded
-  await page.waitForSelector('input[name="session[password]"]')
-  await page.waitForSelector('input[name="session[username_or_email]"]')
-  await page.waitForSelector('div[data-testid="LoginForm_Login_Button"]')
+  await page.waitForSelector('input[type="text"]')
+  await page.waitForSelector('input[type="password"]')
+  await page.waitForSelector('div[role="button"]')
 
   // Login
   await page.type('input[type="text"]', userName, { delay: 5 })
