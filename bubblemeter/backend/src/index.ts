dc3ad8175236273664b538693d6a2a48829f9a1a
Lars Höhener <hoehelar@students.zhaw.ch>
2021-03-25 20:38:37 +0100

    DB connects now at startup of server.

--- bubblemeter/backend/src/index.ts
+++ bubblemeter/backend/src/index.ts
@@ -2,9 +2,25 @@ import BodyParser from 'body-parser';
 import cors from 'cors';
 import Express from 'express';
 import HTTP from 'http';
+import mongoose from 'mongoose';
 
 import { logger } from './logger';
 import { registerRoutes } from './routes';
+
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopolog: true, useFindAndModify: false})
+
+const db = mongoose.connection;
+
+db.on('error', () => {
+    logger.error('Failed to setup connection with DB');
+})
+
+db.once('open', () => {
+    logger.info('Connected successfully to DB!');
+})
+
+
 
 const port = process.env.PORT || 5000;
 
