f5a9b904566cf0c942fa247f851b0cb121bc5a95
Lars Höhener <hoehelar@students.zhaw.ch>
2021-03-30 13:43:44 +0200

    Next test of workflows.

--- bubblemeter/backend/src/controllers/SearchResultController.ts
+++ bubblemeter/backend/src/controllers/SearchResultController.ts
@@ -27,8 +27,7 @@ export class SearchResultController {
         }
 
         const { tweetID, fullText, twittererID, searchTerm, user } = req.body;
-
-        console.log('hello');
+        
 
         const newSearchResult = new SearchResult({
             tweetID,
