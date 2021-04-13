c43fc20ff087b4cb2cadf5bed4c9a6d809ef5eef
Ilbien Paul <paulilb@students.zhaw.ch>
2021-04-13 19:55:36 +0200

    Added db

--- network/collectAllFriends.py
+++ network/collectAllFriends.py
@@ -10,6 +10,7 @@ import pymongo
 import pandas as pd
 from db import is_twitterId_in_db
 from db import insert_edge
+from db import get_all_friends
 
 # relevant DB variables
 client = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -27,27 +28,24 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, 
 
 # start with this user
 userStart = api.get_user(screen_name = 'elbueno_paulo')
-user_Id = [userStart.id]
 
 # get all friends of the userStart
 friends_list = []
 friends = []
-if not is_twitterId_in_db(user_Id, edgeCol):
-    # TODO get all friends of this id from DB
-    # friends_list = ...
+if is_twitterId_in_db(userStart.id, edgeCol):
     print("User is in DB!")
+    friends_list.append(get_all_friends(userStart.id, edgeCol))
 else:
+    print("User is not in DB!")
     try:
-        for page in tweepy.Cursor(api.friends_ids, user_id=user_Id).pages():
+        for page in tweepy.Cursor(api.friends_ids, user_id=userStart.id).pages():
             friends.extend(page)
-            for friend_id in page:
-                friend = api.get_user(user_id = friend_id)
-                insert_edge(userStart.screen_name, user_Id, friend.screen_name, friend.id, edgeCol)
+        for friend_id in friends:
+            friend = api.get_user(user_id = friend_id)
+            insert_edge(userStart.screen_name, userStart.id, friend.screen_name, friend.id, edgeCol)
         friends_list.append(friends)
     except tweepy.TweepError:
         print("error")
-
-#check if friends_count is different or if friend is older than 10 days in DB need to be updated
 
 df = pd.DataFrame(columns=['source','target'])
 
@@ -67,33 +65,20 @@ for userID in friends_list:
     friendCount = friendCount + 1
     print('Friend {:d} of {:d}'.format(friendCount, totalFriends))
     
-    friends = []
-    friends_list = []
-
-    # TODO check if userID is in db
-
-    # fetching the user
-    user = api.get_user(userID)
-
-    # fetching the friends_count
-    friends_count = user.friends_count
-
-    try:
-        for page in tweepy.Cursor(api.friends_ids, user_id=userID).pages():
-            friends.extend(page)
-            
-            # TODO add to database
-            
-            if friends_count >= 5000: #Only take first 5000 friends
-                break
-    except tweepy.TweepError:
-        print("error")
-        continue
-    friends_list.append(friends)
-    temp = pd.DataFrame(columns=['source', 'target'])
-    temp['target'] = friends_list[0]
-    temp['source'] = userID
-    df = df.append(temp)
-    df.to_csv("networkOfFollowers.csv")
-    
-    
+    if is_twitterId_in_db(userID, edgeCol):
+        friends = []
+        # fetching the user
+        user = api.get_user(userID)
+        # defining the friends_count
+        friends_count = user.friends_count
+        try:
+            for page in tweepy.Cursor(api.friends_ids, user_id=userID).pages():
+                friends.extend(page)            
+                if friends_count >= 5000: #Only take first 5000 friends
+                    break
+                for friend_id in friends:
+                    friend = api.get_user(user_id = friend_id)
+                    insert_edge(user.screen_name, user.id, friend.screen_name, friend.id, edgeCol)
+        except tweepy.TweepError:
+            print("error")
+            continue
