4bf12796ad562dca8b849c7e1f4db28bf85d828b
Ilbien Paul <paulilb@students.zhaw.ch>
2021-04-18 19:43:18 +0200

    Removed unnecessary comments added make_analysis

--- normal_server_backend/index.py
+++ normal_server_backend/index.py
@@ -2,68 +2,43 @@
 """
 Created on Sat Apr 17 19:00:55 2021
 
-@author: larsh
+Connect: localhost:5000/
+
+@author: larsh, paulilb1
 """
 
+#flask import
 import flask
-
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-import pymongo
-from datetime import datetime, timedelta
+#sys import, used so that files in other directories are found
+import sys
+sys.path.append("../network")
+
+#mimports
+import pandas as pd
 import json
+from collectAllFriends import process_friends
+from db import get_edges_friends_of_friends
+from network import top_k_of_network_sorted_incoming_degree
+from network import get_all_NR_and_SR_in_network
 
-client = pymongo.MongoClient("")
-
-twitterNetworkDb = client["twitterNetworkDb"]
-
-edgeCol = twitterNetworkDb["twitterEdges"]
-
-
-@app.route('/get_most_influential_nodes/<twitterID>', methods=['GET'])
-def get_most_influential_nodes():
-
-    # 1. get all friends_of_friends (collectAllFriends)
-    
-
-
-    # 2. generate directed graph
-
-
-    # 3. calculate 10 most influential nodes
-
-    
-    # 4. return most influential nodes
-    
-    response = {"statusCode": 200, "body": json.dumps(body)}
-    return response
-
-@app.route('/get_politicians_in_network/<twitterID>', methods=['GET'])
-def get_politicians_in_network():
-
-    # 1. get all friends_of_friends (collectAllFriends)
-
-
-    # 2. generate directed graph
-
-    # 3. get politicians
-
-    
-    # 4. search through politicians
-
-
-    # 5. return politicians in network
-    
-    response = {"statusCode": 200, "body": json.dumps(body)}
-    return response
-
-
-@app.route('/collect_all_friends/<twitterID>', methods=['GET'])
-def collect_all_friends():
-    
-    # 1. get friends_of_friends 
-    response = {"statusCode": 200, "body": json.dumps(body)}
+#call with twitter id
+#returns json object with politicians_in_network and top_ten_most_influential
+@app.route('/make_analysis/<twitterID>')
+def make_analysis(twitterID):
+    process_friends(twitterID)
+    edges = get_edges_friends_of_friends(int(twitterID))
+    edges_df = pd.DataFrame(edges)
+    k = 10
+    ten_most_influential = top_k_of_network_sorted_incoming_degree(k, edges_df)
+    politicians_in_network = get_all_NR_and_SR_in_network(edges_df)
+    result = ten_most_influential.to_json(orient="split")
+    ten_most_influential_parsed = json.loads(result)
+    result = politicians_in_network.to_json(orient="split")
+    politicians_in_network_parsed = json.loads(result)
+    response = {"statusCode": 200, "body": {"politicians_in_network": json.dumps(politicians_in_network_parsed), "top_ten_most_influential": json.dumps(ten_most_influential_parsed) }}
     return response
 
 app.run()
