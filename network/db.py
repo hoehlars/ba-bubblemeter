2f0a8eb80ee79b4c044ad41aa8f04790dc5f4173
Ilbien Paul <paulilb@students.zhaw.ch>
2021-04-18 19:42:05 +0200

    Removed all edgeCol from function parameters, bc it is already implemented as global variable added get_friends

--- network/db.py
+++ network/db.py
@@ -2,36 +2,39 @@
 """
 Created on Sun Apr 11 15:10:19 2021
 
-@author: larsh
+@author: larsh, paulilb1
 """
 
 import pymongo
 from datetime import datetime, timedelta
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-
+client = pymongo.MongoClient("")
 twitterNetworkDb = client["twitterNetworkDb"]
-
 edgeCol = twitterNetworkDb["twitterEdges"]
+politiciansCol = twitterNetworkDb["Politiker"]
 
+def get_politicians():
+    allEntries = politiciansCol.find({}, {"_id": 0, "username": 0})
+    # get NR and SR from db
+    return list(allEntries)
 
-def insert_edge(idFrom, idTo, edgeCol):
+def insert_edge(idFrom, idTo):
     datetime_now = datetime.now()
     edge = { "date": datetime_now, "IDFrom": idFrom, "IDTo": idTo}
     edgeCol.insert_one(edge)
     
 
-def is_twitterId_in_db(twitterId, edgeCol):
+def is_twitterId_in_db(twitterId):
     query = {"IDFrom": twitterId}
     allEntries = edgeCol.find(query)    
     return len(list(allEntries)) != 0
 
-def count_amount_of_friends_in_db(twitterID, edgeCol):
+def count_amount_of_friends_in_db(twitterID):
     query = {"IDFrom": twitterID}
     allEntries = edgeCol.find(query)
     return len(list(allEntries))
 
-def get_date_of_entries(twitterID, edgeCol):
+def get_date_of_entries(twitterID):
     query = {"IDFrom": twitterID}
     allEntriesList = list(edgeCol.find(query))
     if len(allEntriesList) == 0:
@@ -39,17 +42,26 @@ def get_date_of_entries(twitterID, edgeCol):
     else:
         return allEntriesList[0]["date"]
     
-def delete_all_entries(twitterID, edgeCol):
+def delete_all_entries(twitterID):
     query = {"IDFrom": twitterID}
     edgeCol.delete_many(query)
     
     
-def delete_all_entries_before_certain_date(day, month, year, edgeCol):
+def delete_all_entries_before_certain_date(day, month, year):
     d = datetime(year, month, day)
     query = {"date": {"$lt": d}}
     edgeCol.delete_many(query)
     
-def get_edges_friends(twitterID, edgeCol):
+def get_friends(twitterID):
+    query = {"IDFrom": twitterID}
+    # project to IDTo
+    allEntries = edgeCol.find(query, {"_id": 0, "date": 0})
+    friends = []
+    for entry in allEntries:
+        friends.append(entry["IDTo"])
+    return friends
+    
+def get_edges_friends(twitterID):
     query = {"IDFrom": twitterID}
     # project to IDTo
     allEntries = edgeCol.find(query, {"_id": 0, "date": 0})
@@ -59,15 +71,15 @@ def get_edges_friends(twitterID, edgeCol):
     return friends
 
 
-def get_edges_friends_of_friends(twitterID, edgeCol):
+def get_edges_friends_of_friends(twitterID):
     # get friends of specific user
-    friends = get_edges_friends(twitterID, edgeCol)
+    friends = get_edges_friends(twitterID)
     
     friends_of_friends = []
     
     # iterate over friends to get all friends of friends
     for friend in friends:
-        friends_of_friend = get_edges_friends(friend["IDTo"], edgeCol)
+        friends_of_friend = get_edges_friends(friend["IDTo"])
         friends_of_friends.extend(friends_of_friend)
         
     # finally add also friends of specific user 
@@ -75,13 +87,10 @@ def get_edges_friends_of_friends(twitterID, edgeCol):
     
     return friends_of_friends
 
-def delete_all_entries_older_than_10_days(edgeCol):
+def delete_all_entries_older_than_10_days():
     date_10_days_before = datetime.now() - timedelta(days = 10)
     day = date_10_days_before.day
     month = date_10_days_before.month
     year = date_10_days_before.year
     delete_all_entries_before_certain_date(day, month, year, edgeCol)
-    
-#insert_edge('1234', '12345', edgeCol)
-delete_all_entries_older_than_10_days(edgeCol)
-    
+  
