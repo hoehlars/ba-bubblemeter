7ca213ceaf1ad67ebacf6d0bea0fa1d0b07efbed
Lars Höhener <hoehelar@students.zhaw.ch>
2021-04-22 10:40:22 +0200

    Refactors network functions by not creating graph twice. Adds comments.

--- network/network.py
+++ network/network.py
@@ -6,7 +6,6 @@ Created on Wed Apr 14 09:41:06 2021
 """
 
 
-import networkx as nx
 import pandas as pd
 import tweepy
 from db import get_politicians
@@ -21,16 +20,9 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
 
 
-def top_k_of_network_sorted_incoming_degree(k, edges_df):
-    #  turn into a directed graph
-    G = nx.from_pandas_edgelist(edges_df, 'IDFrom', 'IDTo', create_using=nx.DiGraph())
-
-    # sort by incoming degree
-    G_sorted = pd.DataFrame(sorted(G.in_degree, key=lambda x: x[1], reverse=True))
-    G_sorted.columns = ['twitter_id','in_degree']
-    
+def top_k_of_network_sorted_incoming_degree(k, G_sorted_df):
     # get top k
-    G_top_k = G_sorted.head(k)
+    G_top_k = G_sorted_df.head(k)
     
     top_k_df = pd.DataFrame(columns = ['name', 'twitter_handle', 'twitter_id', 'in_degree'])
     
@@ -47,13 +39,8 @@ def top_k_of_network_sorted_incoming_degree(k, edges_df):
     
     return top_k_df
 
-def get_all_NR_and_SR_in_network(edges_df):
-    
-    G = nx.from_pandas_edgelist(edges_df, 'IDFrom', 'IDTo', create_using=nx.DiGraph())
+def get_all_NR_and_SR_in_network(G_sorted_df):
 
-    # sort by incoming degree
-    sorted_network_df = pd.DataFrame(sorted(G.in_degree, key=lambda x: x[1], reverse=True))
-    sorted_network_df.columns = ['twitter_id','in_degree']
     
     politicians = get_politicians()
     
@@ -74,10 +61,10 @@ def get_all_NR_and_SR_in_network(edges_df):
         smartmap = {"smartmap": [x_smartmap, y_smartmap]}
         
         # look if politician is in network
-        if not sorted_network_df[sorted_network_df['twitter_id'] == twitter_id].empty:
+        if not G_sorted_df[G_sorted_df['twitter_id'] == twitter_id].empty:
             
             # search for politician with id, if found --> append it to politicians_df
-            politicians_df = politicians_df.append(sorted_network_df[sorted_network_df['twitter_id'] == twitter_id], ignore_index=True)
+            politicians_df = politicians_df.append(G_sorted_df[G_sorted_df['twitter_id'] == twitter_id], ignore_index=True)
             # add nametwitter handle, party and smartmap coordinates of politician into df
             politicians_df.loc[idx, 'name'] = name
             politicians_df.loc[idx, 'twitter_handle'] = twitter_handle
--- normal_server_backend/index.py
+++ normal_server_backend/index.py
@@ -16,9 +16,11 @@ app.config["DEBUG"] = True
 import sys
 sys.path.append("../network")
 
-#mimports
+#imports
 import pandas as pd
 import json
+
+import networkx as nx
 from collectAllFriends import process_friends
 from db import get_edges_friends_of_friends
 from network import top_k_of_network_sorted_incoming_degree
@@ -28,9 +30,22 @@ from network import get_all_NR_and_SR_in_network
 #returns json object with politicians_in_network and top_ten_most_influential
 @app.route('/make_analysis/<twitterID>')
 def make_analysis(twitterID):
+    
+    # get all friends of friends and check if they are in db or not
     process_friends(twitterID)
+    
+    # get all edges from the db
     edges = get_edges_friends_of_friends(int(twitterID))
+    
+    # create dataframe and graph
     edges_df = pd.DataFrame(edges)
+    G = nx.from_pandas_edgelist(edges_df, 'IDFrom', 'IDTo', create_using=nx.DiGraph())
+
+    # sort by incoming degree
+    G_sorted = pd.DataFrame(sorted(G.in_degree, key=lambda x: x[1], reverse=True))
+    G_sorted.columns = ['twitter_id','in_degree']
+    
+    
     k = 10
     ten_most_influential = top_k_of_network_sorted_incoming_degree(k, edges_df)
     politicians_in_network = get_all_NR_and_SR_in_network(edges_df)
