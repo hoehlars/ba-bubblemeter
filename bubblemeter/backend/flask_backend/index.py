d5f0ddf46765093c4eccc5e2f40873881ad8209e
Daniel Lerch <daniel@daniellerch.ch>
2021-05-08 12:22:45 +0200

    integrates polit-score in backend

--- bubblemeter/backend/flask_backend/index.py
+++ bubblemeter/backend/flask_backend/index.py
@@ -25,7 +25,7 @@ import json
 
 import networkx as nx
 from twitter_access import process_friends
-from db import get_edges_friends_of_friends
+from db import get_edges_friends_of_friends, get_amount_of_politicians_in_db
 from network import top_k_of_network_sorted_incoming_degree
 from network import get_all_NR_and_SR_in_network
 
@@ -62,6 +62,35 @@ def make_analysis(twitterID):
     result = politicians_in_network.to_json(orient="split")
     politicians_in_network_json = json.loads(result)
     response = {"statusCode": 200, "body": {"politicians_in_network": politicians_in_network_json, "top_ten_most_influential": ten_most_influential_json }}
+    return response
+
+@app.route('/polit_score/<twitterID>')
+def polit_score(twitterID):
+    
+    # get all edges from the db
+    edges = get_edges_friends_of_friends(int(twitterID))
+    
+    # create dataframe and graph
+    edges_df = pd.DataFrame(edges)
+    G = nx.from_pandas_edgelist(edges_df, 'IDFrom', 'IDTo', create_using=nx.DiGraph())
+    
+    # sort by incoming degree
+    G_sorted_df = pd.DataFrame(sorted(G.in_degree, key=lambda x: x[1], reverse=True))
+    G_sorted_df.columns = ['twitter_id','in_degree']
+    
+    
+    
+    # get all politicians in network
+    politicians_in_network = get_all_NR_and_SR_in_network(G_sorted_df)
+    
+    size_of_whole_network = G_sorted_df.shape[0]
+    amount_of_politicians_in_network = politicians_in_network.shape[0]
+    amount_of_politicians_in_db = get_amount_of_politicians_in_db()
+    
+    
+    polit_score = amount_of_politicians_in_network / amount_of_politicians_in_db
+    
+    response = {"statusCode": 200, "body": {"amount_of_politicians_in_network": amount_of_politicians_in_network, "amount_of_politicians_in_db": amount_of_politicians_in_db, "size_of_whole_network": size_of_whole_network, "polit_score": polit_score}}
     return response
 
 app.run()
