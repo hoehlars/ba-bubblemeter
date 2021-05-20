5d8b65930d2793dc6b7486e40bf20335cf81bfb6
Ilbien Paul <paulilb@students.zhaw.ch>
2021-05-20 17:35:41 +0200

    Added comments and made sections for each collections methods

--- bubblemeter/backend/flask_backend/db.py
+++ bubblemeter/backend/flask_backend/db.py
@@ -18,64 +18,7 @@ politiciansCol = twitterNetworkDb[config['POLITICIANS_COL_NAME']]
 analyzedCol = twitterNetworkDb[config['ANALYZED_USERS_NAME']]
 requestQueueCol = twitterNetworkDb[config['QUEUE']]
 
-
-def get_politicians():
-    allEntries = politiciansCol.find({}, {"_id": 0, "username": 0})
-    # get all politicans from db
-    return list(allEntries)
-
-def get_amount_of_politicians_in_db():
-    allEntries = politiciansCol.find({}, {"_id": 0, "username": 0})
-    return len(list(allEntries))
-
-def insert_edge(idFrom, idTo):
-    datetime_now = datetime.now()
-    edge = { "date": datetime_now, "IDFrom": idFrom, "IDTo": idTo}
-    edgeCol.insert_one(edge)
-    
-def insert_analyzed_user(twitterID, twitterHandle, twitterName, friends_count):
-    datetime_now = datetime.now()
-    edge = { "date": datetime_now, "twitterId": twitterID, "twitterHandle": twitterHandle, "twitterName": twitterName, "friends": friends_count}
-    analyzedCol.insert_one(edge)
-    
-def get_analyzed_users():
-    #returns all items in Collection
-    allEntries = analyzedCol.find().sort("date",1)
-    users = []
-    for entry in allEntries:
-        user = {"name": entry["twitterName"], "handle": entry["twitterHandle"], "id": entry["twitterId"]}
-        users.append(user)
-    return users
-
-def is_twitterHandle_analyzed(twitterHandle):
-    query = {"twitterHandle": twitterHandle}
-    allEntries = analyzedCol.find(query)    
-    return len(list(allEntries)) != 0
-
-def is_twitterHandle_in_queue(twitterHandle):
-    query = {"twitterHandle": twitterHandle}
-    allEntries = requestQueueCol.find(query)    
-    return len(list(allEntries)) != 0
-
-def is_queue_empty():
-    allEntries = requestQueueCol.find()
-    count = len(list(allEntries))
-    return count == 0
-    
-def insert_request_in_queue(twitterHandle):
-    datetime_now = datetime.now()
-    edge = { "date": datetime_now, "twitterHandle": twitterHandle}
-    requestQueueCol.insert_one(edge) 
-
-def get_next_request_from_queue():
-    # 1 for oldest, -1 for newest
-    nextUserDict = requestQueueCol.find().sort("date",1).limit(1)
-    nextUser = nextUserDict[0]
-    return nextUser['twitterHandle']
-
-def remove_user_from_queue(twitterHandle):
-    query = {"twitterHandle": twitterHandle}
-    requestQueueCol.delete_one(query)
+#----- GENERAL -----
 
 def is_twitterId_in_db(twitterId):
     query = {"IDFrom": twitterId}
@@ -115,3 +58,73 @@ def get_edges_friends_of_friends(twitterID):
     friends_of_friends.extend(friends)
     
     return friends_of_friends
+
+def insert_edge(idFrom, idTo):
+    datetime_now = datetime.now()
+    edge = { "date": datetime_now, "IDFrom": idFrom, "IDTo": idTo}
+    edgeCol.insert_one(edge)
+
+#----- POLITICIANS -----
+
+def get_politicians():
+    allEntries = politiciansCol.find({}, {"_id": 0, "username": 0})
+    # get all politicans from db
+    return list(allEntries)
+
+def get_amount_of_politicians_in_db():
+    allEntries = politiciansCol.find({}, {"_id": 0, "username": 0})
+    return len(list(allEntries))
+   
+#-----ANALYZED USERS----- 
+   
+def insert_analyzed_user(twitterID, twitterHandle, twitterName, twitterProfileImage, friends_count):
+    datetime_now = datetime.now()
+    edge = { "date": datetime_now, "twitterId": twitterID, "twitterHandle": twitterHandle, "twitterName": twitterName, "twitterProfileImage": twitterProfileImage, "friends": friends_count}
+    analyzedCol.insert_one(edge)
+    
+def get_analyzed_users():
+    #returns all items in Collection
+    allEntries = analyzedCol.find().sort("date",1)
+    users = []
+    for entry in allEntries:
+        user = {"name": entry["twitterName"], "handle": entry["twitterHandle"], "id": entry["twitterId"]}
+        users.append(user)
+    return users
+
+def is_twitterHandle_analyzed(twitterHandle):
+    query = {"twitterHandle": twitterHandle}
+    allEntries = analyzedCol.find(query)    
+    return len(list(allEntries)) != 0
+
+#-----REQUEST QUEUE-----
+
+def insert_request_in_queue(twitterHandleOrTwitterID):
+    datetime_now = datetime.now()
+    edge = { "date": datetime_now, "twitterHandleOrTwitterID": twitterHandleOrTwitterID}
+    requestQueueCol.insert_one(edge) 
+    
+#returns the oldest request from the queue
+def get_next_request_from_queue():
+    # 1 for oldest, -1 for newest
+    nextUserDict = requestQueueCol.find().sort("date",1).limit(1)
+    nextUser = nextUserDict[0]
+    return nextUser['twitterHandle']
+
+def get_request_queue_length():
+    #returns all items in Collection
+    allEntries = requestQueueCol.find()
+    return len(allEntries)
+
+def remove_user_from_queue(twitterHandle):
+    query = {"twitterHandle": twitterHandle}
+    requestQueueCol.delete_one(query)
+
+def is_twitterHandle_in_queue(twitterHandle):
+    query = {"twitterHandle": twitterHandle}
+    allEntries = requestQueueCol.find(query)    
+    return len(list(allEntries)) != 0
+
+def is_queue_empty():
+    allEntries = requestQueueCol.find()
+    count = len(list(allEntries))
+    return count == 0
