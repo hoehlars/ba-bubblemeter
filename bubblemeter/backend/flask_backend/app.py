0ba12c57332a535f75482be6676e49e7e5637853
DESKTOP-5T6CLGF\larsh <lars.hoehener@gmx.ch>
2021-05-24 17:03:31 +0200

    Removes all calculation from the flask backend to the make analysis module.

--- bubblemeter/backend/flask_backend/app.py
+++ bubblemeter/backend/flask_backend/app.py
@@ -15,34 +15,10 @@ app = flask.Flask(__name__)
 
 CORS(app)
 
-#sys import, used so that files in other directories are found
-import sys
-sys.path.append("../network")
-
 #imports
-import pandas as pd
-
-from db import get_edges_friends_of_friends, get_amount_of_politicians_in_db
-from db import insert_request_in_queue
-from db import is_twitterHandle_analyzed
-from db import is_twitterHandle_in_queue
-from db import get_analyzed_users
-from db import get_request_queue_length
-from network import top_k_of_network_sorted_incoming_degree
-from network import get_all_politicians_in_network
-from network import generate_graph
-from centroid import compute_centroid_top_k_percent
-from centroid import compute_inside_outside_circle
-from helpers import df_to_json
 from flask_swagger_ui import get_swaggerui_blueprint
 
-MOST_INFLUENTIAL_TOP_COUNT = 10
-MOST_INFLUENTIAL_PARTY_POLIT_COUNT = 100
-CENTROID_TOP_K_PERCENT_POLIT = 5
-RADIUS_AROUND_CENTROID = 16
-DF_ROW_COUNT = 0
-
-
+from db import get_analysis_of_user_analyzed
 
 
 SWAGGER_URL = '/docs'
@@ -57,153 +33,10 @@ swagger_ui_blueprint = get_swaggerui_blueprint(
 
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
-@app.route('/make_analysis/<twitterID>')
-def make_analysis(twitterID):
-    
-    # get all edges from the db
-    edges = get_edges_friends_of_friends(int(twitterID))
-    
-    # create dataframe and graph
-    edges_df = pd.DataFrame(edges)
-    
-    G_sorted_df = generate_graph(edges_df)
-    
-    ten_most_influential = top_k_of_network_sorted_incoming_degree(MOST_INFLUENTIAL_TOP_COUNT, G_sorted_df)
-    
-    # check if the twitter user itself is in the top ten list
-    if not ten_most_influential[ten_most_influential.twitter_id == int(twitterID)].empty:
-    # get top 11 most influential nodes, remove twitter user self
-        ten_most_influential = top_k_of_network_sorted_incoming_degree(MOST_INFLUENTIAL_TOP_COUNT + 1, G_sorted_df)
-        ten_most_influential = ten_most_influential[ten_most_influential.twitter_id != int(twitterID)]
-    
-    politicians_in_network = get_all_politicians_in_network(G_sorted_df)
-    
-    ten_most_influential_json = df_to_json(ten_most_influential)
-    politicians_in_network_json = df_to_json(politicians_in_network)
-    
-    response = {"statusCode": 200, "body": {"politicians_in_network": politicians_in_network_json, "top_ten_most_influential": ten_most_influential_json }}
-    return response
-
-@app.route('/request_analysis/<twitterHandleOrTwitterID>/<email>')
-def request_analysis(twitterHandleOrTwitterID, email):
-    
-    if is_twitterHandle_analyzed(twitterHandleOrTwitterID):
-        errorMsg = "User has already been analyzed."
-        response = {"statusCode": 200, "body": {"msg": errorMsg}}
-        return response
-    
-    if is_twitterHandle_in_queue(twitterHandleOrTwitterID):
-        errorMsg = "User has already been requested and is still in the queue."
-        response = {"statusCode": 200, "body": {"msg": errorMsg}}
-        return response
-    
-    #inserts user request in queue
-    insert_request_in_queue(twitterHandleOrTwitterID, email)
-    
-    successMsg = "User analysis of " + twitterHandleOrTwitterID + " has been requestet"
-    response = {"statusCode": 200, "body": {"msg": successMsg}}
-    return response
-
-@app.route('/request_analyzed_users')
-def request_analysed_users():
-    
-    analyzed_users = get_analyzed_users()
-    
-    if len(analyzed_users) == 0:
-        response = {"statusCode": 200, "body": {"msg": "No user has been analyzed yet."}}
-        return response
-    
-    response = {"statusCode": 200, "body": {"analyzed_users": analyzed_users }}
-    return response
-
-@app.route('/request_queue_length')
-def request_queue_length():
-    
-    queue_length = get_request_queue_length()
-    
-    response = {"statusCode": 200, "body": {"queue_length": queue_length }}
-    return response
-
-@app.route('/polit_score/<twitterID>')
-def polit_score(twitterID):
-    
-    edges = get_edges_friends_of_friends(int(twitterID))
-    
-    edges_df = pd.DataFrame(edges)
-    G_sorted_df = generate_graph(edges_df)
-    
-    politicians_in_network = get_all_politicians_in_network(G_sorted_df)
-    
-    size_of_whole_network = G_sorted_df.shape[DF_ROW_COUNT]
-    amount_of_politicians_in_network = politicians_in_network.shape[DF_ROW_COUNT]
-    amount_of_politicians_in_db = get_amount_of_politicians_in_db()
-    
-    polit_score = amount_of_politicians_in_network / amount_of_politicians_in_db
-    
-    response = {"statusCode": 200, "body": {"amount_of_politicians_in_network": amount_of_politicians_in_network, "amount_of_politicians_in_db": amount_of_politicians_in_db, "size_of_whole_network": size_of_whole_network, "polit_score": polit_score}}
-    return response
-
-@app.route('/most_influential_party/<twitterID>')
-def most_influential_party(twitterID):
-    
-    edges = get_edges_friends_of_friends(int(twitterID))
-    
-    edges_df = pd.DataFrame(edges)
-    G_sorted_df = generate_graph(edges_df)
-    
-    politicians_in_network = get_all_politicians_in_network(G_sorted_df)
-    
-    politicians_in_network_top_100 = politicians_in_network.head(MOST_INFLUENTIAL_PARTY_POLIT_COUNT)
-    
-    parties = {}
-    
-    # calculate score for each party with politicians in top 100 
-    for index, row in politicians_in_network_top_100.iterrows():
-        party = row["partyAbbreviation"]
-        in_degree = row["in_degree"]
-        
-        if not party in parties.keys():
-            parties[party] = in_degree
-        else:
-            parties[party] = parties[party] + in_degree
-            
-    
-    response = {"statusCode": 200, "body": {"parties": parties}}
-    return response
-
-
-@app.route('/centroid/<twitterID>')
-def centroid(twitterID):
-    
-    edges = get_edges_friends_of_friends(int(twitterID))
-    
-    edges_df = pd.DataFrame(edges)
-    
-    G_sorted_df = generate_graph(edges_df)
-    
-    coordinates = compute_centroid_top_k_percent(G_sorted_df, CENTROID_TOP_K_PERCENT_POLIT)
-
-    response = {"statusCode": 200, "body": {"x": coordinates["x"], "y": coordinates["y"]}}
-    return response
-
-@app.route('/inner_outer_circle/<twitterID>')
-def inner_outer_circle(twitterID):
-        
-    edges = get_edges_friends_of_friends(int(twitterID))
-    
-    edges_df = pd.DataFrame(edges)
-    
-    G_sorted_df = generate_graph(edges_df)
-    
-    politicians_in_network = compute_inside_outside_circle(G_sorted_df, CENTROID_TOP_K_PERCENT_POLIT, RADIUS_AROUND_CENTROID)
-        
-    politicians_inside = politicians_in_network[politicians_in_network["isInside"] == True]
-    politicians_outside = politicians_in_network[politicians_in_network["isInside"] == False]
-
-    politicians_inside_json = df_to_json(politicians_inside)
-    politicians_outside_json = df_to_json(politicians_outside)
-    
-    response = {"statusCode": 200, "body": {"radius": RADIUS_AROUND_CENTROID, "politicians_inside": politicians_inside_json, "politicians_outside": politicians_outside_json}}
+@app.route('/get_analysis/<twitterID>')
+def get_analysis(twitterID):
+    analysis = get_analysis_of_user_analyzed(twitterID)
+    response = {"statusCode": 200, "body": {"analysis": analysis}}
     return response
 
 
--- bubblemeter/backend/process-friends/make_analysis.py
+++ bubblemeter/backend/process-friends/make_analysis.py
@@ -5,5 +5,70 @@ Created on Mon May 24 16:13:48 2021
 @author: larsh
 """
 
-def make_analysis():
+from db import get_edges_friends_of_friends
+from network import generate_graph
+from network import top_k_of_network_sorted_incoming_degree
+from network import get_all_politicians_in_network
+from network import compute_polit_score_analysis
+from network import compute_most_influential_party
+from helpers import df_to_json
+from centroid import compute_centroid_top_k_percent
+from centroid import compute_inside_outside_circle
+import pandas as pd
+
+
+MOST_INFLUENTIAL_TOP_COUNT = 10
+MOST_INFLUENTIAL_PARTY_POLIT_COUNT = 100
+CENTROID_TOP_K_PERCENT_POLIT = 5
+RADIUS_AROUND_CENTROID = 16
+
+def make_analysis(twitterID):
+    
+    # create dict for returning
+    analysis = {}
+    
+    # get all edges from the db
+    edges = get_edges_friends_of_friends(int(twitterID))
+    edges_df = pd.DataFrame(edges)
+    
+    # create graph
+    G_sorted_df = generate_graph(edges_df)
+    
+    # ------------ ten_most_influential --------------
+    ten_most_influential = top_k_of_network_sorted_incoming_degree(MOST_INFLUENTIAL_TOP_COUNT, G_sorted_df)
+    analysis["ten_most_influential"] = df_to_json(ten_most_influential)
+    
+    # ------------ politicians in network ------------
+    politicians_in_network = get_all_politicians_in_network(G_sorted_df)
+    analysis["politicians_in_network"] = df_to_json(politicians_in_network)
+    
+    # ------------ polit_score -----------------------
+    polit_score_analysis = compute_polit_score_analysis(G_sorted_df)
+    analysis["amount_of_politicians_in_network"] = polit_score_analysis["amount_of_politicians_in_network"]
+    analysis["amount_of_politicians_in_db"] = polit_score_analysis["amount_of_politicians_in_db"]
+    analysis["size_of_whole_network"] = polit_score_analysis["size_of_whole_network"]
+    analysis["polit_score"] = polit_score_analysis["polit_score"]
+    
+    # ------------ most influential party ------------
+    parties = compute_most_influential_party(G_sorted_df, MOST_INFLUENTIAL_PARTY_POLIT_COUNT)
+    analysis["parties"] = df_to_json(parties)
+    
+    # ------------ centroid --------------------------
+    coordinates = compute_centroid_top_k_percent(G_sorted_df, CENTROID_TOP_K_PERCENT_POLIT)
+    analysis["x"] = coordinates["y"]
+    analysis["y"] = coordinates["y"]
+    
+    # ------------ inner outer circle ----------------
+    politicians_in_network = compute_inside_outside_circle(G_sorted_df, CENTROID_TOP_K_PERCENT_POLIT, RADIUS_AROUND_CENTROID)
+
+    politicians_inside = politicians_in_network[politicians_in_network["isInside"] == True]
+    politicians_outside = politicians_in_network[politicians_in_network["isInside"] == False]
+
+    politicians_inside_json = df_to_json(politicians_inside)
+    politicians_outside_json = df_to_json(politicians_outside)
+    analysis["politicians_inside"] = politicians_inside_json
+    analysis["politicians_outside"] = politicians_outside_json
+    
+    return analysis
+    
     
