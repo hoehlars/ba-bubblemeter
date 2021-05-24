b6095ebe8c981a31f22ec83e1db562a99149b608
DESKTOP-5T6CLGF\larsh <lars.hoehener@gmx.ch>
2021-05-24 17:04:44 +0200

    Makes analysis now before inserting the user to the db.

--- bubblemeter/backend/process-friends/twitter_access.py
+++ bubblemeter/backend/process-friends/twitter_access.py
@@ -11,6 +11,7 @@ from db import is_twitterId_in_db
 from db import insert_edge
 from db import get_friends
 from db import insert_analyzed_user
+from make_analysis import make_analysis
 import os
 
 key_in_use = 1
@@ -129,7 +130,9 @@ def process_friends_of_friends(user, friends):
                 change_keys()
                 continue
         friend_position = friend_position + 1
-    insert_analyzed_user(user.id, user.screen_name, user.name, user.profile_image_url_https, user.friends_count)
+    
+    analysis = make_analysis(user.id)
+    insert_analyzed_user(user.id, user.screen_name, user.name, user.profile_image_url_https, user.friends_count, analysis)
     print("finished")
             
 def get_user_from_id(twitter_id):
