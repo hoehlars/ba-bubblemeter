4a31101668628f9070bc999a644a3ba5cc4748dc
Daniel Lerch <daniel@daniellerch.ch>
2021-05-31 14:48:29 +0200

    adds default profile pic to bubble-forming lists
    - but only if src=none
    - doesnt account for 404 on smartvote

--- newfront/src/components/List.js
+++ newfront/src/components/List.js
@@ -4,6 +4,9 @@ export default function List({ list }) {
   return (
     <ul className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
       {list.map((user) => {
+        const src =
+          user[6] ||
+          'https://abs.twimg.com/sticky/default_profile_images/default_profile.png'
         return (
           <li key={user[0]} className='mb-2'>
             <a
@@ -15,7 +18,7 @@ export default function List({ list }) {
               <figure>
                 <img
                   className='rounded-full w-20 h-20 object-cover m-auto'
-                  src={user[6]}
+                  src={src}
                   alt={`${user[3]}s Profile Pic`}
                 />
                 <figcaption className='font-light'>@{user[3]}</figcaption>
