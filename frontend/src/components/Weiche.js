5f6fb0d864b331d4d6e8fa95ae8846430d6f09ed
Daniel Lerch <daniel@daniellerch.ch>
2021-06-06 21:47:31 +0200

    expands weiche links to  button area

--- frontend/src/components/Weiche.js
+++ frontend/src/components/Weiche.js
@@ -3,12 +3,16 @@ import { NavLink } from 'react-router-dom'
 export default function Weiche() {
   return (
     <ul className='transition-colors '>
-      <li className='px-6 py-4 m-auto mb-6 font-semibold text-center text-pink-600 transition-colors border-2 border-pink-600 rounded-full md:w-96 hover:bg-pink-600 hover:text-white'>
-        <NavLink to='/userinput'>Nutzer*in erfassen</NavLink>
-      </li>
-      <li className='px-6 py-4 m-auto mb-6 font-semibold text-center text-pink-600 transition-colors border-2 border-pink-600 rounded-full md:w-96 hover:bg-pink-600 hover:text-white'>
-        <NavLink to='/userselection'>Liste anzeigen</NavLink>
-      </li>
+      <NavLink to='/userinput'>
+        <li className='px-6 py-4 m-auto mb-6 font-semibold text-center text-pink-600 transition-colors border-2 border-pink-600 rounded-full md:w-96 hover:bg-pink-600 hover:text-white'>
+          Nutzer*in erfassen
+        </li>
+      </NavLink>
+      <NavLink to='/userselection'>
+        <li className='px-6 py-4 m-auto mb-6 font-semibold text-center text-pink-600 transition-colors border-2 border-pink-600 rounded-full md:w-96 hover:bg-pink-600 hover:text-white'>
+          Liste anzeigen
+        </li>
+      </NavLink>
     </ul>
   )
 }
